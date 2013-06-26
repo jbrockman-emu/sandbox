2510f0b31c2987aba51456c211b72a4aabceb26a
Jay Brockman <jbrockman@emutechnology.com>
2013-06-26 17:25:58 -0400

    Changed to printf

--- hello/src/hello.c
+++ hello/src/hello.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!!\n"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
