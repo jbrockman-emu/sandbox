aa09ea7e3ca2984691e7bc5098786ef3d63e59cb
Jay Brockman <jbrockman@emutechnology.com>
2013-07-18 11:55:39 -0400

    Added CircularBuffer

---
+++ java-examples/src/org/jbrockman_emu/java_examples/CircularBuffer.java
@@ -0,0 +1,48 @@
+package org.jbrockman_emu.java_examples;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class CircularBuffer implements Iterable<String> {
+	LinkedList<String> list = new LinkedList<String>();
+	int nextIndex = 0;
+	
+	public CircularBuffer() {
+		// default constructor
+	}
+	
+	public String toString() {
+		return(nextIndex + ":" + list.toString());
+	}
+	
+	public void add(String s) {
+		ListIterator<String> it = list.listIterator(nextIndex);
+		it.add(s);
+		nextIndex = it.nextIndex() % list.size();
+	}
+	
+	public String next() {
+		ListIterator<String> it = list.listIterator(nextIndex);
+		String result = it.next();
+		nextIndex = it.nextIndex() % list.size();
+		return result;
+	}
+	
+	public void remove() {
+		ListIterator<String> it = list.listIterator(nextIndex);
+		it.next();
+		it.remove();
+		if (list.size() != 0) {
+			nextIndex = it.nextIndex() % list.size();
+		}
+	}
+	
+	public Iterator<String> iterator() {
+		return list.listIterator();
+	}
+	
+	public boolean empty() {
+		return list.size() == 0;
+	}
+}
---
+++ java-examples/tests/org/jbrockman_emu/java_examples/TestCircularBuffer.java
@@ -0,0 +1,100 @@
+package org.jbrockman_emu.java_examples;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class TestCircularBuffer {
+	CircularBuffer b;
+	
+	void setup() {
+		b = new CircularBuffer();
+		b.add("A");
+		b.add("B");
+		b.add("C");
+		b.add("D");
+		b.add("E");
+		print("setup");
+	}
+	
+	void setupNull() {
+		b = new CircularBuffer();
+		print("setupNull");
+	}
+	
+	void print(String s) {
+		System.out.println(b + " " + s);
+	}
+
+	@Test
+	public void testNext() {
+		System.out.println("---- testNext ----");
+		setup();
+		for (int i = 0;  i < 8;  i++) {
+			System.out.println(b.next());
+			print("next");
+		}
+		setupNull();
+		try {
+			b.next();
+		} catch (NoSuchElementException e) {
+			System.err.println("Caught NoSuchElementException: " + e.getMessage());
+		} finally {
+			print("next tried on empty buffer");
+		}
+	}
+
+	@Test
+	public void testRemove() {
+		System.out.println("---- testRemove ----");
+		setup();
+		b.remove();
+		print("remove A");
+		b.remove();
+		print("remove B");
+		b.next();
+		b.remove();
+		print("remove D");
+		b.remove();
+		print("remove E");
+		assertFalse(b.empty());
+		b.remove();
+		print("remove C");
+		assertTrue(b.empty());
+	}
+	
+	@Test
+	public void testRemoveSingleton() {
+		System.out.println("---- testRemoveSingleton ----");
+		b = new CircularBuffer();
+		b.add("A");
+		print("singleton");
+		b.next();
+		print("next");
+		b.remove();
+		print("remove A");
+	}
+
+	@Test
+	public void testIterator() {
+		System.out.println("---- testIterator ----");
+		setup();
+		for (String s : b) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+	}
+	
+	@Test
+	public void testAdd() {
+		System.out.println("---- testAdd ----");
+		setup();
+		b.add("X");
+		print("add X at penultimate");
+		b.next();
+		b.add("Y");
+		print("add Y at head");
+	}
+
+}
